a54e82e1d1e43e6d3ccdb13f4b8d81d1fd52508f
Alexandre Gaigalas <alexandre@gaigalas.net>
2014-05-06 01:15:34 -0300

    Better UI, leastFactor fix, coprimes and distinct factors, legend

--- js/app.js
+++ js/app.js
@@ -1,12 +1,67 @@
 "use strict";
 
+
+    var mobiusCache = {};
+
+    var mobiusFunc = function(num) {
+        num = Math.abs(num);
+        if (num in mobiusCache) {
+            return mobiusCache[num];
+        }
+        var factors = factor(num);
+        var unique  = factors.filter(function(itm,i,a){
+            return ( i==a.indexOf(itm) );
+        });
+        if (factors.length !== unique.length) {
+            return mobiusCache[num] = [factors, 0];
+        } else if(!!(num && !(num%2))) {
+            return mobiusCache[num] = [factors, 1];
+        } else {
+            return mobiusCache[num] = [factors, -1];
+        }
+    }
+
+
+    function factor(n) {
+     if (isNaN(n) || !isFinite(n) || n%1!=0 || n==0) return[n];
+     var minFactor = leastFactor(n);
+     if (n==minFactor) return [n];
+     return factor(n/minFactor).concat(minFactor);
+    }
+
+
+    // find the least factor in n by trial division
+    function leastFactor(n) {
+     if (isNaN(n) || !isFinite(n)) return NaN;  
+     if (n==0) return 0;  
+     if (n%1 || n*n<2) return 1;
+     if (n%2==0) return 2;  
+     if (n%3==0) return 3;  
+     if (n%5==0) return 5;  
+     var m = Math.sqrt(n);
+     for (var i=7;i<=m;i+=30) {
+      if (n%i==0)      return i;
+      if (n%(i+4)==0)  return i+4;
+      if (n%(i+6)==0)  return i+6;
+      if (n%(i+10)==0) return i+10;
+      if (n%(i+12)==0) return i+12;
+      if (n%(i+16)==0) return i+16;
+      if (n%(i+22)==0) return i+22;
+     }
+     return n;
+    }
+
+
 (function(win, doc, nav) {
+    var primes = [];
     var canvas, context, width, height, num, size, resolution, divisor;
     var Spiral = function() {
         var radius = (resolution / 2) || 6;
         var radiusPow = Math.pow(width / radius, 2);
         var halfRadius = radius / 2;
         var twiceRadius =  radius * 2;
+        var halfWidth = (width / 2);
+        var halfHeight = (height / 2);
         //init is -1,-1
         var point = {
             x : 0,
@@ -32,25 +87,25 @@
             }
         }
         var drawPoint = function(opt) {
-            opt = opt || {};
-            opt.x = opt.x || 0;
-            opt.y = opt.y || 0;
-            opt.radius = opt.radius || halfRadius;
+            opt.x = opt.x;
+            opt.y = opt.y;
+            opt.radius = opt.radius;
             context.beginPath();
-            var coord = {
-                x : (width / 2) + (opt.x * twiceRadius),
-                y : (height / 2) + (-1 * opt.y * twiceRadius)
-            };
-            context.arc(coord.x, coord.y, opt.radius, 0, 2 * Math.PI, false);
+            context.arc(
+                halfWidth + (opt.x * twiceRadius), 
+                halfHeight + (-1 * opt.y * twiceRadius), 
+                opt.radius, 
+                1, 
+                0, 
+                false
+            );
             context.fillStyle = opt.color;
             context.fill();
             return true;
         };
         var setDirection = function(point, dirIndex) {
-            if ((dirIndex >= 0 || dirIndex < 4)) {
-                point.x += direction[dirIndex].x;
-                point.y += direction[dirIndex].y;
-            }
+            point.x += direction[dirIndex].x;
+            point.y += direction[dirIndex].y;
             return point;
         }
         var nextDirection = function(dir) {
@@ -63,75 +118,92 @@
         var generate = function() {
             var directionSteps = 1;
             var directionIndex = 0;
-            var seqs = {
-                yellow: [],
-                cyan:   [],
-                pink:   [],
-                white:  []
-            };
             var i = num;
-            drawPoint({
-                x : point.x,
-                y : point.y,
-                color : 'rgba(255, 255, 255, 0.25)',
-                radius : 8
-            });
             var cont = true;
             do {
                 for (var k = 0; k < 2; k++) {
                     for (var ds = 0; ds < directionSteps; ds += 1) {
                         i = i+1/divisor;
-                        var floorI = Math.floor(i);
-                        //increment number
+                        if (i>0) {
+                            var stepI = Math.floor(i);
+                        } else {
+                            var stepI = Math.ceil(i);
+                        }
                         point = setDirection(point, directionIndex);
 
+                        var computedAlpha = 0.5;
+                        var computedRadius = 7;
                         var pointToDraw = {};
-                        var mu = mobiusFunc(floorI);
-                        //var mainF = (Math.sqrt(Math.sqrt(i)));
-                        var alpha = 1;//( 1 / mainF );
-                        var rads = 6;//mainF;
-                        if (radiusP) {
-                            rads = Math.sqrt(floorI) / Math.sqrt(canvasS/Math.PI);
+                        if (stepI==0 || stepI==1) {
+                            drawPoint({
+                                x : point.x,
+                                y : point.y,
+                                color : 'rgba(0, 0, 0, '+ computedAlpha +')',
+                                radius : computedRadius
+                            });
+                            continue;
                         }
+                        var mu = mobiusFunc(stepI);
 
-                        if (alphaP) {
-                            alpha = 1 / (Math.sqrt(floorI) / Math.sqrt(canvasS*Math.PI))
-                        }
-
-                        if (mu[0].length == 1) {  // one factor
-                            if (parseInt(i) === floorI) { // prime
-                                pointToDraw = {
-                                    x : point.x,
-                                    y : point.y,
-                                    color : 'rgba(0, 0, 0, '+ alpha +')',
-                                    radius : rads
-                                };
-                            }
-                        } else if (mu[1] === -1) {  // red odd unique factors
+                        if (mu[1] === -1) {  // red odd unique factors
                             pointToDraw = {
                                 x : point.x,
                                 y : point.y,
-                                color : 'rgba(255, 50, 50, '+ alpha +')',
-                                radius : rads
+                                color : 'rgba(200, 0, 0, '+ computedAlpha +')',
+                                radius : computedRadius
                             };
                         } else if (mu[1] === 1) { // blue even unique factors
                             pointToDraw = {
                                 x : point.x,
                                 y : point.y,
-                                color : 'rgba(50, 50, 255, '+ alpha +')',
-                                radius : rads
+                                color : 'rgba(0, 0, 200, '+ computedAlpha +')',
+                                radius : computedRadius
                             };
 
                         } else {                  // green non unique factors
                             pointToDraw = {
                                 x : point.x,
                                 y : point.y,
-                                color : 'rgba(50, 255, 50, '+ alpha +')',
-                                radius : rads
+                                color : 'rgba(0, 200, 0, '+ computedAlpha +')',
+                                radius : computedRadius
                             };
+                        }
+
+                        if (mu[0].length == 1) {  // one factor
+                            if (parseInt(i) === stepI) { // prime
+                                pointToDraw = {
+                                    x : point.x,
+                                    y : point.y,
+                                    color : 'rgba(255, 255, 255, '+ computedAlpha*1.5 +')',
+                                    radius : computedRadius
+                                };
+                                primes.push(i);
+                            }
                         } 
+
+                        
+                        if (mu[0].length == 2 // two factors, semi primes
+                            && mobiusFunc(mu[0][0])[0].length ==1
+                            && mobiusFunc(mu[0][1])[0].length ==1) {
+                            drawPoint({
+                                x : point.x,
+                                y : point.y,
+                                color : 'rgba(255, 255, 255, '+ computedAlpha +')',
+                                radius : computedRadius
+                            });
+                        } else if (mu[0].length == 3 // two factors, semi primes
+                            && mobiusFunc(mu[0][0])[0].length ==1
+                            && mobiusFunc(mu[0][1])[0].length ==1
+                            && mobiusFunc(mu[0][2])[0].length ==1) {
+                            drawPoint({
+                                x : point.x,
+                                y : point.y,
+                                color : 'rgba(255, 255, 255, '+ computedAlpha +')',
+                                radius : computedRadius / 2
+                            });
+                        }
+
                         drawPoint(pointToDraw);
- 
 
                         if (i >= (radiusPow)) {
                             cont = false;
@@ -140,7 +212,7 @@
                     directionIndex = nextDirection(directionIndex);
                 }
                 directionSteps += 1;
-            } while (cont) ;
+            } while (cont);
         }
         return {
             generate : generate
@@ -178,9 +250,14 @@
               };
     })();
 
-    var i = 1;
-    var canvasS = parseInt(document.getElementById('canvasSize').value);
+    var start = document.getElementById('start').value, canvasS;
     
+    divisor = document.getElementById('divisor').value;
+    document.getElementById('divisor').onchange = function () {
+        divisor = this.value;
+    };
+
+
     var sizeInit =function () {
         canvasS = parseInt(document.getElementById('canvasSize').value);
         canvas = doc.getElementById("canvas");
@@ -191,14 +268,11 @@
         width = parseInt(canvas.width, 10);
         height = parseInt(canvas.height, 10)
         context = canvas.getContext("2d");
+        start = document.getElementById('start').value;
     };
     sizeInit();
 
     document.getElementById('canvasSize').onchange = sizeInit;
-    divisor = document.getElementById('divisor').value;
-    document.getElementById('divisor').onchange = function () {
-        divisor = this.value;
-    };
 
     var radiusP = document.getElementById('size').checked;
     var alphaP = document.getElementById('alpha').checked;
@@ -210,6 +284,9 @@
             radiusP = false;
         }
     };
+    document.getElementById('start').onchange = function () {
+        start = this.value;
+    };
     document.getElementById('alpha').onchange = function () {
         if (this.checked) {
             alphaP = true;
@@ -218,59 +295,16 @@
         }
     };
 
-    (function animloop(){
-      generate(i++, divisor);
-      requestAnimFrame(animloop);
-    })();
+    function animloop(){
+      if (start <= canvasS) {
+        generate(++start, divisor);
+        requestAnimFrame(animloop);
+      }
+    };
+
+    document.getElementById('anim').onclick = function (e) {
+        animloop();
+        e.preventDefault();
+    };
 
 })(window, document, navigator);
-
-    var mobiusCache = {};
-
-    var mobiusFunc = function(num) {
-        if (num in mobiusCache) {
-            return mobiusCache[num];
-        }
-        var factors = factor(num);
-        var unique  = factors.filter(function(itm,i,a){
-            return mobiusCache[num] = ( i==a.indexOf(itm) );
-        });
-        if (factors.length !== unique.length) {
-            return mobiusCache[num] = [factors, 0];
-        } else if(!!(num && !(num%2))) {
-            return mobiusCache[num] = [factors, 1];
-        } else {
-            return mobiusCache[num] = [factors, -1];
-        }
-    }
-
-
-    function factor(n) {
-     if (isNaN(n) || !isFinite(n) || n%1!=0 || n==0) return[n];
-     var minFactor = leastFactor(n);
-     if (n==minFactor) return [n];
-     return factor(n/minFactor).concat(minFactor);
-    }
-
-
-    // find the least factor in n by trial division
-    function leastFactor(n) {
-     if (isNaN(n) || !isFinite(n)) return NaN;  
-     if (n==0) return 0;  
-     if (n%1 || n*n<2) return 1;
-     if (n%2==0) return 2;  
-     if (n%3==0) return 3;  
-     if (n%5==0) return 5;  
-     var m = Math.sqrt(n);
-     for (var i=7;i<=m;i+=30) {
-      if (n%i==0)      return i;
-      if (n%(i+4)==0)  return i+4;
-      if (n%(i+6)==0)  return i+6;
-      if (n%(i+10)==0) return i+10;
-      if (n%(i+12)==0) return i+12;
-      if (n%(i+16)==0) return i+16;
-      if (n%(i+22)==0) return i+22;
-      if (n%(i+24)==0) return i+24;
-     }
-     return n;
-    }
